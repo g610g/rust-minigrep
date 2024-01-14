1cce38f6a5ee3c2a9b5e48eff13333cdce2237f4
g610g <gio.gonzales@carsu.edu.ph>
2024-01-14 13:15:24 +0800

    added reading file to a specified file path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-use std::env;
+use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let query = &args[1];
+    let file_path = &args[2];
+    let content = extract_file_path_contents(file_path);
+    println!("File text:\n{}", content);
+}
+
+fn extract_file_path_contents(file_path: &str) -> String{
+    let contents = fs::read_to_string(file_path)
+        .expect("Error reading the file at the specified path");
+    contents
 }
