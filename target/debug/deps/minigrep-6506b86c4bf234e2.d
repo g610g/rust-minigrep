25e79db6aa2d022ba4b9b35072544e65a963dfbb
g610g <gio.gonzales@carsu.edu.ph>
2024-01-14 12:55:55 +0800

    adding the env module and args function into scope

---
+++ target/debug/deps/minigrep-6506b86c4bf234e2.d
@@ -0,0 +1,5 @@
+/home/g6i1o0/Documents/rust/minigrep/target/debug/deps/minigrep-6506b86c4bf234e2: src/main.rs
+
+/home/g6i1o0/Documents/rust/minigrep/target/debug/deps/minigrep-6506b86c4bf234e2.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/g6i1o0/Documents/rust/minigrep/target/debug/minigrep: /home/g6i1o0/Documents/rust/minigrep/src/main.rs
