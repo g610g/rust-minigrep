8fd4258cb1f6f926c3951df3748092bc94b743aa
g610g <gio.gonzales@carsu.edu.ph>
2024-01-15 19:40:47 +0800

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::{env, fs};
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    let content = extract_file_path_contents(file_path);
-    println!("File text:\n{}", content);
+struct Config{
+    query:String,
+    file_path:String
 }
-
 fn extract_file_path_contents(file_path: &str) -> String{
     let contents = fs::read_to_string(file_path)
         .expect("Error reading the file at the specified path");
     contents
+}
+fn parse_config(configs:&[String])-> Config{
+    let query = configs[1].clone();
+    let file_path = configs[2].clone();
+    Config{query, file_path}
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
+    let content = extract_file_path_contents(&config.file_path);
+    println!("File text:\n{}", content);
 }
---
+++ target/debug/deps/minigrep-27b24e56fdb4b6b8.d
@@ -0,0 +1,5 @@
+/home/g6i1o0/Documents/rust/minigrep/target/debug/deps/libminigrep-27b24e56fdb4b6b8.rmeta: src/main.rs
+
+/home/g6i1o0/Documents/rust/minigrep/target/debug/deps/minigrep-27b24e56fdb4b6b8.d: src/main.rs
+
+src/main.rs:
