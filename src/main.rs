b83bcdd060d825e0fc22de564b03da68e9ef41f2
g610g <gio.gonzales@carsu.edu.ph>
2024-01-16 00:07:20 +0800

    refactored to create struct method new for constructing

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,22 @@ struct Config{
     query:String,
     file_path:String
 }
+impl Config{
+    fn new(configs: &[String]) -> Config{
+        Config{
+            query:configs[1].clone(),
+            file_path:configs[2].clone()
+        }
+    }
+}
 fn extract_file_path_contents(file_path: &str) -> String{
     let contents = fs::read_to_string(file_path)
         .expect("Error reading the file at the specified path");
     contents
 }
-fn parse_config(configs:&[String])-> Config{
-    let query = configs[1].clone();
-    let file_path = configs[2].clone();
-    Config{query, file_path}
-}
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     let content = extract_file_path_contents(&config.file_path);
     println!("File text:\n{}", content);
 }
