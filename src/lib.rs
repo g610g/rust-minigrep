8b4278fda03c50074d08991ad707a4c0ff390de6
g610g <gio.gonzales@carsu.edu.ph>
2024-01-17 13:00:03 +0800

    added insensitive case feature and env variable setting

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use std::{fs};
+use std::{fs,env};
 use std::error::Error;
 
 pub struct Config{
     query:String,
-    file_path:String
+    file_path:String,
+    ignore_case:bool
 }
 impl Config{
     pub fn build(configs: &[String]) -> Result<Config, &'static str>{
@@ -12,15 +13,31 @@ impl Config{
        }
        let query = configs[1].clone();
        let file_path = configs[2].clone();
-       Ok(Config{query, file_path})
+       let ignore_case = env::var("IGNORE_CASE").is_ok();
+       Ok(Config{query, file_path, ignore_case})
     }
 }
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(&config.file_path)?;
-    for line in search(&config.query, &contents){
+    let result  = if config.ignore_case{
+        insensitive_search(&config.query, &contents)
+    }else{
+        search(&config.query, &contents)
+    };
+    for line in result {
         println!("{line}");
     }
     Ok(())
+}
+pub fn insensitive_search<'a>(query:&str, contents:&'a str)-> Vec<&'a str>{
+    let mut result: Vec<&str> = vec![];
+    let query = query.to_lowercase();
+    for line in contents.lines(){
+            if line.to_lowercase().contains(&query){
+                result.push(line);
+            }
+    }
+    return result;
 }
 pub fn search<'a >(query:&str, contents:&'a str)-> Vec<&'a str>{
     let mut result: Vec<&str> = vec![];
@@ -36,7 +53,7 @@ pub fn search<'a >(query:&str, contents:&'a str)-> Vec<&'a str>{
 mod tests{
     use super::*;
     #[test]
-    fn search_test(){
+    fn search_test_sensitive(){
         let query = "duct";
         let contents = "\
 Rust:
@@ -44,4 +61,13 @@ safe, fast, productive
 Pick three:";
         assert_eq!(vec!["safe, fast, productive"], search(query, contents));
     }
+    #[test]
+    fn search_test_insensitive(){
+        let query = "Io";
+        let contents = "\
+gio gonzales
+standard IO library
+just a dummy text i cant think of anything";
+        assert_eq!(vec!["gio gonzales", "standard IO library"], insensitive_search(query, contents));
+}
 } 
