b9445aeac3e23ced5aa89aedbb4c982f4c8a1c08
g610g <gio.gonzales@carsu.edu.ph>
2024-01-17 11:58:56 +0800

    finished search logic and functionality with testing

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,19 @@ impl Config{
 }
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(&config.file_path)?;
+    for line in search(&config.query, &contents){
+        println!("{line}");
+    }
     Ok(())
 }
 pub fn search<'a >(query:&str, contents:&'a str)-> Vec<&'a str>{
-    return vec![];
+    let mut result: Vec<&str> = vec![];
+    for line in contents.lines(){
+        if line.contains(query){
+            result.push(line);
+        } 
+    }
+    result
 }
 
 #[cfg(test)]
@@ -30,9 +39,9 @@ mod tests{
     fn search_test(){
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive
-        Pick three:";
+Rust:
+safe, fast, productive
+Pick three:";
         assert_eq!(vec!["safe, fast, productive"], search(query, contents));
     }
 } 
